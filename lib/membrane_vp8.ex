f0692427c0836e1c5a6efc8ab118d11c94570440
Maks Stachowiak <maks.stachowiak@swmansion.com>
2021-11-22 14:09:38 +0100

    new fields

--- lib/membrane_vp8.ex
+++ lib/membrane_vp8.ex
@@ -3,5 +3,19 @@ defmodule Membrane.VP8 do
   This module implements struct describing a VP8 encoded video stream.
   """
 
+  use Ratio
+
+  @type width_t :: integer
+  @type height_t :: integer
+  @type timebase_t :: Ratio
+  @type frame_count_t :: integer
+
+  @type t :: %__MODULE__{
+    width: width_t,
+    height: height_t,
+    timebase: timebase_t,
+    frame_count: frame_count_t
+  }
+
   defstruct []
 end
